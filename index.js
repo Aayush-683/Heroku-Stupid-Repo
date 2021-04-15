cb38d1756f748aa00c8efd072b5708ba7aa5e1e7
BlackKnight683 <75422863+BlackKnight683@users.noreply.github.com>
2021-04-15 12:19:16 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const discord = require("discord.js")
+const bot = new Discord.Client()
+
+bot.on('ready', async bot => {
+    bot.user.setActivity("Something....")
+    console.log("Logged in successfully :D")
+})
+
+bot.on('message', async message => {
+    if(message.content == "!ping") {
+        message.channel.send("pong!")
+    }
+})
+
+bot.login("TOKEN")
